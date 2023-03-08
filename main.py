fd7fba964a8a07f509daeb6cdcbc48d3ef65dc0f
Cole Peterson <cole@epeterson.net>
2023-03-07 21:15:09 -0800

    adding initial snake body

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+## Snake Game
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+def new_snake_part():
+    snake_part = Turtle(shape="square")
+    snake_part.color("white")
+    return snake_part
+
+def gen_initial_snake():
+    new_start=0
+    for i in range(3):
+        snake_piece = new_snake_part()
+        snake_piece.setposition(new_start, 0)
+        new_start = snake_piece.xcor() - 20
+    return snake
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Cole')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.exitonclick()
