457b31ae2bb5e7f3d6c881a1993c8d07b542f1d9
Cole Peterson <cole@epeterson.net>
2023-03-10 06:28:08 -0800

    totally independent updates I came up with myself, not outside help

--- main.py
+++ main.py
@@ -14,12 +14,23 @@ def new_snake_part():
 
 def gen_initial_snake():
     new_start=0
+    segments = list()
     for i in range(3):
         snake_piece = new_snake_part()
+        snake_piece.penup()
+        screen.tracer(5)
         snake_piece.setposition(new_start, 0)
         new_start = snake_piece.xcor() - 20
-    return snake
+        segments.append(snake_piece)
+    return segments
+
+snake = gen_initial_snake()
+game_is_on = True
+while game_is_on:
+    for segment in snake:
+        segment.forward(20)
 
 
+# TODO: Add time library to control how quickly snake is moving, update how the screen is updating
 
 screen.exitonclick()
