9026ae8994729cf178b3d7cab0023c7ac7b3b720
Cole Peterson <cole@epeterson.net>
2023-03-11 06:28:31 -0800

    Updating methods and attributes for oop functionality

--- snake.py
+++ snake.py
@@ -1,29 +1,56 @@
 from turtle import Screen, Turtle
+import time
+
+UP=90
+DOWN=270
+LEFT=180
+RIGHT=0
+
 class Snake:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.segments = list()
+        self.new_snake_part()
+        self.head = self.segments[0]
 
-    def new_snake_part(self, segment):
-        self.segment = segment
-        self.segment = Turtle(shape="square")
-        self.segment.color("black")
-        self.segment.goto(-20,-20)
+    def new_snake_part(self):
+        """
+        Creates a new segment of the snake and appends to the list
+        for easy tracking
+        """
+        segment = Turtle(shape="square")
+        segment.color("white")
+        segment.penup()
+        self.segments.append(segment)
+    def connect_snake_part(self,seg_count,new_start):
+        """
+        Add new segment to the snake
+        :param seg_count:  segments to add
+        :param new_start: x-coordinate to generate segment at
+        :return: Updates segment list for class instance
+        """
+        screen = Screen()
+        for i in range(seg_count):
+            self.new_snake_part()
+            self.segments[i].setposition(new_start, 0)
+            new_start = self.segments[i].xcor() - 20
 
-    # def get_intial_snake(self):
-    #     new_start = 0
-    #     self.color("white")
-    #     segments = list()
-    #     for i in range(3):
-    #         snake_piece = new_snake_part()
-    #         snake_piece.penup()
-    #         screen.tracer(5)
-    #         snake_piece.setposition(new_start, 0)
-    #         new_start = snake_piece.xcor() - 20
-    #         segments.append(snake_piece)
-    #     return segments
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
-screen = Screen()
-screen.setup(width=600, height=600)
-t = Snake("cole")
-t.new_snake_part(1)
-screen.exitonclick()
+
+# screen = Screen()
+#
+# screen.setup(width=600, height=600)
+# screen.bgcolor("black")
+# t = Snake()
+# screen.exitonclick()
