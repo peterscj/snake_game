5509ffce38130cb2cc064bbe8f9bd02c8f4eed25
Cole Peterson <cole@epeterson.net>
2023-03-10 16:01:21 -0800

    updated with some basic code to move snake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 ## Snake Game
 
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -27,10 +28,15 @@ def gen_initial_snake():
 snake = gen_initial_snake()
 game_is_on = True
 while game_is_on:
-    for segment in snake:
-        segment.forward(20)
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(snake)-1, 0,-1):
+        # Grab x & y coordinates of previous snake segment and have it current one go there
+        new_x = snake[seg_num-1].xcor()
+        new_y = snake[seg_num-1].ycor()
+        snake[seg_num].goto(new_x, new_y)
 
+    snake[0].forward(20)
 
-# TODO: Add time library to control how quickly snake is moving, update how the screen is updating
 
 screen.exitonclick()
